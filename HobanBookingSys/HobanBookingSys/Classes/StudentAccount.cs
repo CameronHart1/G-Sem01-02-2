c8e25014ba9fbd6bceb12f45c94034a3bea18033
roussoren <82788920+roussoren@users.noreply.github.com>
2021-05-06 17:16:58 +1000

    Add files via upload

---
+++ HobanBookingSys/HobanBookingSys/Classes/StudentAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class StudentAccount : Account
+    {
+        private String Name;
+        private String Email;
+        private String BookedSlots;
+        
+
+        public StudentAccount(String _KeyIdentifier, bool IsOrganizer, String Name, String Email, String BookedSlots) : base(_KeyIdentifier, false)
+        {
+            
+            this.Name = Name;
+            this.Email = Email;
+            this.BookedSlots = BookedSlots;
+        }
+
+        public override string ToString() => $"{KeyId},{Name},{Email},{BookedSlots}";
+        
+
+    }
+}
