822216de3193d3f56bd622399befb15eb9f13bd1
CameronHart1 <82789385+CameronHart1@users.noreply.github.com>
2021-05-06 17:23:14 +1000

    StringArrayConverter

---
+++ HobanBookingSys/HobanBookingSys/Classes/StringArrayConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HobanBookingSys.Classes
+{
+    class StringArrayConverter
+    {
+        public static string ConvertToString(string[] inArray)
+        {
+            string tempString = "";
+            foreach (string entry in inArray)
+            {
+                tempString
+            }
+
+                return tempString;
+        }
+    }
+}
